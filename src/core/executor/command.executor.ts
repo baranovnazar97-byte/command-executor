914d6f693d43a45e176fb49efcf509c2a0f974ce
Nazar Baranov <baranovnazar97@gmail.com>
2026-07-29 20:14:27 +0300

    add CommandExecutor abstract class and implement Template Method pattern

---
+++ src/core/executor/command.executor.ts
@@ -0,0 +1,27 @@
+import { ChildProcessWithoutNullStreams } from 'child_process';
+import { IStreamLogger } from '../handlers/stream-logger.interface';
+import { ICommandExec } from './command.types';
+
+export abstract class CommandExecutor<T> {
+  constructor(protected logger: IStreamLogger) {}
+
+  protected abstract prompt(): Promise<T>;
+
+  protected abstract build(input: T): ICommandExec;
+
+  protected abstract spawn(
+    command: ICommandExec,
+  ): Promise<ChildProcessWithoutNullStreams>;
+
+  protected abstract processStream(
+    stream: ChildProcessWithoutNullStreams,
+    logger: IStreamLogger,
+  ): Promise<void>;
+
+  public async execute() {
+    const res = await this.prompt();
+    const command = this.build(res);
+    const stream = await this.spawn(command);
+    await this.processStream(stream, this.logger);
+  }
+}
